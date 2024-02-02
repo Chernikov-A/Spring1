f8412ee2b2746e4b25cb3de6721df3411b0574ba
Chernikov_A <chernikovomg@mail.ru>
2024-02-02 17:46:40 +0700

    new

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,6 +1,9 @@
 package org.example;
 
 import java.util.Objects;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Person {
     String firstName;
@@ -18,20 +21,28 @@ public class Person {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
-        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+        return new EqualsBuilder()
+                .append(age,person.age)
+                .append(firstName,person.firstName)
+                .append(lastName,person.lastName)
+                .isEquals();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName, age);
+        return new HashCodeBuilder()
+                .append(age)
+                .append(lastName)
+                .append(firstName)
+                .toHashCode();
     }
 
     @Override
     public String toString() {
-        return "Person{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
+        return new ToStringBuilder(this)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("age",age)
+                .toString();
     }
 }
